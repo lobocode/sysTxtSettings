a22e98e02272f8c192b4cb3c9bbe97ef40667f67
Vitor Lobo <lobocode@gmail.com>
2016-02-29 11:06:51 -0300

    Modificando estrutura
    Adicionei condicionais de segurança para impedir baixar arquivos
    duplicados e outras coisas já comentadas no arquivo.

--- autopilot/autopilot.sh
+++ autopilot/autopilot.sh
@@ -16,27 +16,17 @@ if [ "$uid" != "root" ]; then
 
 else
 
-	#Arquivos do host
-	#files="*.tar.gz"
-
+	#Arquivos que serão gerados a partir do index.html baixado no host por wget -q
 	fileOutput=files.html
-
 	fileFilterNv1=filtro.txt
 
 	# Endereço onde estarão os arquivos
 	#HostUrlFiles="http://www.car.ti.lemaf.ufla.br/bkp/releases_prod/"
 
-	# Procurar arquivos .tar.gz atuais
-	#searchFiles="$(find . -maxdepth 1 -mtime +0 -type f -iname \$files)"
-
-	# Fazendo análise de arquivos no host e gerando um file.html com os respectivos dados do index 
-	# sem o código html
-	#wget -q $HostUrlFiles | xargs sed -e 's/<[^>]*>//g' index.html > $fileOutput && sed -i 1,2d $fileOutput
+	# Baixa o index.html direto do host, limpa os dados de tag's em html com sed, gera um arquivo limpo chamado files.html e remove o primeiro arquivo index.html baixado.
+	wget -q $HostUrlFiles | xargs sed -e 's/<[^>]*>//g' index.html > $fileOutput && sed -i 1,2d $fileOutput && rm -f index.html
 	
-	#Removendo lixo
-	#rm index.html
-	
-	#Data recente
+	# Gravar a data no formato mostrado no arquivo files.html
 	dataFiles=`date +%d-%b-%Y`
 
 	# Procurar arquivos .tar.gz com manipulação de strings na data correspondente
@@ -45,17 +35,33 @@ else
 	# Gera um filtro de texto que contém apenas os .tar.gz na cada correspondente
 	for x in $searchTxtFiles
 	do
+		# Ler o files.html e isola apenas os dados com *.tar.gz com a data presente
 		echo $x >> $fileFilterNv1 
 	
 	done	
 
+
 	# Isola por manipulação de strings os arquivos a serem baixados
 	downloadFiles="$(grep ".*.tar.gz" $fileFilterNv1)"
 
-	for i in $downloadFiles 
+	# Procurar arquivos .tar.gz com find na pasta que estamos trabalhando
+	verifyFiles="$(find . -maxdepth 1 -mtime +0 -type f -iname '*.tar.gz')"
+
+	for i in $verifyFiles
 	do
-		echo "$(wget -c $i)" && rm -f $fileFilterNv1
+		if [[ -f $i ]]; then
+			# Se existem arquivos *.tar.gz na pasta
+			echo -e "\nJá existem arquivos referentes à data $dataFiles nesta pasta\n"
+		else
+			# Se não existem
+			for x in $downloadFiles
+			do
+				# Baixar arquivos *.tar.gz direto do host e remover filtro
+				echo -e "$(wget -c $x)" && rm -f $fileFilterNv1
+		fi
 	done
+
+
 
 	
 
